470f9d0d2c20c9a8c64a930ac248be2c502aae3a
Maanav <maanavnagda2197@gmail.com>
2026-09-05 17:15:44 +0100

    Bump version to 4.0 for rebrand release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.example.mntnode"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
